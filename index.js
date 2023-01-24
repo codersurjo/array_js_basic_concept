adb1aa7296c669a5b16df71c132c7b58c58cbd8d
codersurjo <codersurjo@gmail.com>
2023-01-25 01:47:41 +0600

    index.js commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [10,12,24,45,67,23,45];
+
+
+//get element bY index
+var second = numbers[1];
+
+// console.log(numbers[0]);
+// console.log(numbers[2]);
+// console.log(second);
+
+
+// set Element by index 
+numbers[0]=65;
+console.log(numbers);
